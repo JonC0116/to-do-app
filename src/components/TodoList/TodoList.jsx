469e7e300f8c4d5bd555ba484e60069e257272a6
JonC0116 <jeonghwanchoi@wifi-wpa-cw-140-193-247-75.wifi.umanitoba.ca>
2023-01-20 16:00:43 -0600

    Create Todo components, and replce each li tag by new Todo components

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import { TfiTrash } from "react-icons/tfi";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -10,12 +10,20 @@ export default function TodoList() {
   ]);
 
   const handleAdd = (todo) => setTodos([...todos, todo]);
-
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
